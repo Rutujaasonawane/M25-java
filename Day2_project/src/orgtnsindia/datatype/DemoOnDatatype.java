99242c18d18e46c65557817ef0ed0f8f86e2dfbf
Admin <Admin@DESKTOP-6RQIJD3>
2022-09-29 12:46:07 +0530

    first-commit

---
+++ Day2_project/src/orgtnsindia/datatype/DemoOnDatatype.java
@@ -0,0 +1,10 @@
+package orgtnsindia.datatype;
+//program for understanding datatype
+public class DemoOnDatatype {
+
+	public static void main(String[] args) {
+		int xyz=15;
+		System.out.println(xyz);
+	}
+
+}
