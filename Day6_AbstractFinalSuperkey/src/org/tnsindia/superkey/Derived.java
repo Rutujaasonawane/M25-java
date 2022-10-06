3a1e61e01ee68833c48755144dd4db731d109c2c
Admin <Admin@DESKTOP-6RQIJD3>
2022-10-06 14:30:38 +0530

    firstcommit

---
+++ Day6_AbstractFinalSuperkey/src/org/tnsindia/superkey/Derived.java
@@ -0,0 +1,18 @@
+package org.tnsindia.superkey;
+
+public class Derived {
+	//default constructor
+			Derived()
+			{
+				System.out.println("Child Class constructor-Default");
+			}
+			//Parameterized Constructor
+			Derived(int i)
+			{
+				super();
+				System.out.println("Child Class constructor-Parameterized");
+
+			}
+
+
+}
