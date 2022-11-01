0d534411950dfe88212fb4878643fcaa0098311d
Admin <Admin@DESKTOP-6RQIJD3>
2022-11-01 13:51:13 +0530

    firstcommit

---
+++ Day13_DateTimeAPI/src/org/tnsindia/datetime/DemoOnLocalDateAndTime.java
@@ -0,0 +1,14 @@
+package org.tnsindia.datetime;
+
+import java.time.LocalDateTime;
+
+public class DemoOnLocalDateAndTime {
+
+	public static void main(String[] args) {
+		LocalDateTime obj = LocalDateTime.now();
+		
+		System.out.println(obj);
+		
+	}
+
+}
