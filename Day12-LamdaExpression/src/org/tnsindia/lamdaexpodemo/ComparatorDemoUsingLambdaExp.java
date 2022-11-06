13d1889b19b0be0e573db3a895971828b7786b26
Admin <Admin@DESKTOP-6RQIJD3>
2022-11-06 21:48:38 +0530

    firstcommit

---
+++ Day12-LamdaExpression/src/org/tnsindia/lamdaexpodemo/ComparatorDemoUsingLambdaExp.java
@@ -0,0 +1,23 @@
+package org.tnsindia.lamdaexpodemo;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+//comparator is an interface
+public class ComparatorDemoUsingLambdaExp {
+
+	public static void main(String[] args) {
+		Comparator<String>obj=(s1,s2)->Integer.compare(s1.length(), s2.length());
+		String str[]= {"Rutuja","Shubham","Rohit","Ganesh"};
+		//arrange into the ascending order
+		Collections.sort(Arrays.asList(str),obj);
+		//enhanced for loop to return the string
+		for(String itr:str)
+		{
+			System.out.println(itr+" ");
+		}
+
+	}
+
+}
---
+++ Day12-LamdaExpression/src/org/tnsindia/lamdaexpodemo/FunctionalInterfaceExample.java
@@ -0,0 +1,10 @@
+package org.tnsindia.lamdaexpodemo;
+
+
+public interface FunctionalInterfaceExample {
+
+	//1.Functional interface contains exactly one abstract method
+	//abstract method
+	//void max(int a,int b);
+	int max(int a,int b);
+}
